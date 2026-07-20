82b46dbe9bcced5a4a156a847b8dafa5357dad0d
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-07-20 08:19:03 +0700

    konp_test (KONPsurv) With (In) R Software

---
+++ konp_test/konp_test.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# KONP tests are K-sample Omnibus Non-Proportional hazards tests for right-censored data Use konp_test (KONPsurv) With (In) R Software
+install.packages("KONPsurv")
+
+library("KONPsurv")
+# Estimation KONP tests are K-sample Omnibus Non-Proportional hazards tests for right-censored data Use konp_test (KONPsurv) With (In) R Software
+konp_test = read.csv("https://raw.githubusercontent.com/timbulwidodostp/konp_test/main/konp_test/konp_test.csv",sep = ";")
+konp_test <- konp_test(konp_test$time, konp_test$status, konp_test$group, n_perm=10^3)
+konp_test
+# KONP tests are K-sample Omnibus Non-Proportional hazards tests for right-censored data Use konp_test (KONPsurv) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
